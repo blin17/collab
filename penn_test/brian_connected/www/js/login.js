97efb1351abdf18718e9d709f034b66ce5ec265a
Brian Lin <blin17@gmail.com>
2013-09-08 05:20:08 -0400

    brian Cnnnected

---
+++ penn_test/brian_connected/www/js/login.js
@@ -0,0 +1,64 @@
+var usersRef = new Firebase('https://studywithme.firebaseio.com/users/');
+
+$("#login").submit(function() {
+	console.log("login credentials submitted");
+	var email = $("#email").val();
+	var password = $("#password").val();
+	var success = 0;
+	if (email == '') {
+		$("span").text("Please enter your email").show().fadeOut(2000);
+		return false;
+	}
+
+	if (password == '') {
+		$("span").text("Please enter your password").show().fadeOut(2000);
+		return false;
+	}	
+	
+	var chatRef = new Firebase('https://studywithme.firebaseio.com/');  	
+  	var auth = new FirebaseSimpleLogin(chatRef, function(error, user) {
+		if (error) {
+		   // an error occurred while attempting login
+		   console.log(error);
+		   $("span").text("Login Error").show().fadeOut(500);
+		   return false;
+		} else if (user) {
+		  // user authenticated with Firebase
+		  console.log('User ID: ' + user.id + ', Provider: ' + user.provider);
+		  localStorage.setItem('userID', user.id);
+			$("span").text("SUCCESS").show().fadeOut(500);		
+			var userRef = usersRef.child(user.id).on('value', function(names) {
+				localStorage.setItem('name', names.val().name);
+				window.location.replace("displayClass.html");	
+			});
+
+		} else {
+		  // user is logged out
+		}
+		return false;
+	});
+	auth.login('password', {
+		email: email,
+		password: password,
+		rememberMe: true 
+	});
+ 	/*
+ 	var user = baseRef.child('users/10').on('value', function(snapshot){
+                var classes = snapshot.val().classes;
+                console.log(classes);
+                if(classes.indexOf("CS3110") > -1)
+                {
+                    console.log("YESS IT WORKS");
+                }
+            });
+            console.log(classes);            
+        }
+
+	return false;
+	*/
+	console.log("hello");
+	if (success == 1){
+		return true;
+	}
+	return false;
+ });
---
+++ penn_test/brian_connected/www/js/message.js
@@ -0,0 +1,68 @@
+
+// Get a reference to the root of the chat data.
+var messagesRef = new Firebase('https://studywithme.firebaseio.com/messages/');
+
+function post(){
+var lat = 0;
+var lon = 0;
+    if (navigator.geolocation){
+		navigator.geolocation.getCurrentPosition(showPosition, error);
+    }
+ }
+
+function update(){
+	var UID = localStorage.getItem('userID'); //userID 
+	var CID = localStorage.getItem('classID'); //classID
+  var name = localStorage.getItem('name');
+	var text = $('#messageInput').val();
+	var time = getTime();
+	messagesRef.push({userID:UID, classID: CID, lat:lat, lon:lon, text:text, time:time, name:name});
+	$('#messageInput').val('');
+}
+
+function showPosition(position, cb)
+{
+	lat = position.coords.latitude;
+	lon = position.coords.longitude;
+	update();
+}
+
+function error()
+{
+  update();
+}
+
+function getTime(){
+    var date = new Date();
+    var hour = date.getHours();
+    var min = date.getMinutes();
+    var sec = date.getSeconds();
+    var half = '';
+    var time = '';
+    if(hour < 12)
+    {
+      if(hour == 0)
+      {
+        hour = 12;
+      }
+        half = 'AM';
+    }
+    else
+    {
+      if(hour != 12)
+      {
+        hour = hour - 12;  
+      }
+      half = 'PM';
+    }
+    if(min < 10)
+    {
+      time = '' + hour + ':' + '0' + min + ':' + sec + half;
+    }
+    else
+    {
+      time = '' + hour + ':' + min + ':' + sec + half;
+    }
+	return time;
+}
+
