3d429bce354965d751e5495f0923a10a336d30d5
moey509 <mso32@cornell.edu>
2013-09-07 21:19:24 -0400

    small logic changes to test and register

--- penn_test/matt_test/login.js
+++ penn_test/matt_test/login.js
@@ -1,12 +1,54 @@
 $("#login").submit(function() {
-	alert("login credentials submitted");
+	console.log("login credentials submitted");
 	var email = $("#email").val();
 	var password = $("#password").val();
-	auth.login("password", {
-		email: email,
-		password: password,
-		rememberMe: true
+	
+	if (email == '') {
+		$("span").text("Please enter your email").show().fadeOut(2000);
+		return false;
+	}
+
+	if (password == '') {
+		$("span").text("Please enter your password").show().fadeOut(2000);
+		return false;
+	}	
+
+	var chatRef = new Firebase('https://studywithme.firebaseio.com/');  	
+  	var auth = new FirebaseSimpleLogin(chatRef, function(error, user) {
+		if (error) {
+		   // an error occurred while attempting login
+		   console.log(error);
+		   $("span").text("Login Error").show().fadeOut(2000);
+		   return false;
+		} else if (user) {
+		  // user authenticated with Firebase
+		  console.log('User ID: ' + user.id + ', Provider: ' + user.provider);
+			$("span").text("SUCCESS").show().fadeOut(2000);
+		  return true;
+
+		} else {
+		  // user is logged out
+		}
 	});
 
-	return true;
+	auth.login('password', {
+		email: email,
+		password: password,
+		rememberMe: true 
+	});
+ 	/*
+ 	var user = baseRef.child('users/10').on('value', function(snapshot){
+                var classes = snapshot.val().classes;
+                console.log(classes);
+                if(classes.indexOf("CS3110") > -1)
+                {
+                    console.log("YESS IT WORKS");
+                }
+            });
+            console.log(classes);            
+        }
+
+	return false;
+	*/
+	return false;
  });
